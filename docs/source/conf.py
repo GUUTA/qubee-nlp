c21cc4789e6f56339857eb82875c78f43fd3d797
GUUTA <guutatesema@gmail.com>
2025-12-28 00:20:20 +0100

    new file added

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,28 +1,24 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))  # project root relative to source/
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'conf.py'
-copyright = '2025, Guta Tesema Tufa'
+project = 'qubee-nlp'
 author = 'Guta Tesema Tufa'
+copyright = '2025, Guta Tesema Tufa'
 release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints',
+    'myst_parser',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
